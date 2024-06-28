3e08a2e1c6be5a2f9cdb76065e84a0e75bb25e9d
Sorge1941 <2240858981@qq.com>
2024-06-28 15:11:58 +0800

    springcache

--- src/main/java/com/itluchao/reggie/controller/DishController.java
+++ src/main/java/com/itluchao/reggie/controller/DishController.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -115,12 +117,13 @@ public class DishController {
 
     // 修改菜品
     @PutMapping("")
+    @CacheEvict(value = "dishCache",allEntries = true)
     public R<String> updateDish(@RequestBody DishDto dishDto) {
         // 修改
         dishService.updateDish(dishDto);
         //清理缓存数据
         String key="dish_"+dishDto.getCategoryId()+"_1";
-        redisTemplate.delete(key);
+       // redisTemplate.delete(key);
 
         return R.success("修改成功");
     }
@@ -135,24 +138,26 @@ public class DishController {
 
     // 删除菜品
     @DeleteMapping("")
+    @CacheEvict(value = "dishCache",allEntries = true)
     public R<String> DeleteDish(@RequestParam long ids) {
 
-        dishService.DeleteDish(ids);
+       // dishService.DeleteDish(ids);
         return R.success("删除成功");
     }
 
     // 返回套餐菜品
     @GetMapping("/list")
+    @Cacheable(value = "dishCache" ,key = "#dish.categoryId+'_'+#dish.status")
     public R<List<DishDto>> fanhui(Dish dish){
         List<DishDto> dto=null;
-        //设置key
+       /*  //设置key
         String key="dish_"+dish.getCategoryId()+"_"+dish.getStatus();
 
         //从redis获取数据
         dto=  (List<DishDto>) redisTemplate.opsForValue().get(key);
         if(dto!=null){//取到数据，不用查数据库
             return R.success(dto);
-        } 
+        }  */
         //编辑查询条件
         QueryWrapper<Dish> queryWrapper=new QueryWrapper<Dish>();
         if(dish.getCategoryId()!=0)
@@ -177,7 +182,7 @@ public class DishController {
             return dishDto;
         }).collect(Collectors.toList());
         //第一次查询赋值
-        redisTemplate.opsForValue().set(key,dto,60,TimeUnit.MINUTES);
+      //  redisTemplate.opsForValue().set(key,dto,60,TimeUnit.MINUTES);
 
 
         return R.success(dto);
